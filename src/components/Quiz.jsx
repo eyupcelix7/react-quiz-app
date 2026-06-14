e839efe8ca9518c26399d773470d026c404225dc
eyupcelix7 <eyupturkey641@gmail.com>
2026-06-14 23:02:47 +0300

    Soruları karıştırma ve temel mantık yazma işlemi.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompletedImg from "../assets/quiz-complete.png";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswer) => {
       return [...prevAnswer, selectedAnswer];
     });
   }
+  if (quizIsCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImg} alt="Quiz Tamamlandı" />
+        <h2>Quiz Tamamlandı!</h2>
+      </div>
+    );
+  }
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div className="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
